9003bff422c557f4961fd09b3430e57c8d5d9600
GovindaRadha <newuser@example.com>
2024-11-01 14:54:08 +0530

    del

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,8 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.example.app',
   appName: 'PWA',
-  webDir: 'outgit status
-  ',
+  webDir: 'out',
   // bundledWebRuntime: false,
 };
 
