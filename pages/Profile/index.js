1fa81462cf980e134f9d03b9baf64f16fd48e423
Application-Saab <harsha10889@gmail.com>
2025-05-02 11:29:08 +0530

    ui fix

--- pages/Profile/index.js
+++ pages/Profile/index.js
@@ -161,7 +161,7 @@ const ProfileUpdate = () => {
                 required
               >
                 <option value="">Select Job Profile</option>
-                <option value="" job_type="2">Chef</option>
+                {/* <option value="" job_type="2">Chef</option> */}
                 <option value="Decorator" job_type="1">Decorator</option>
                 <option value="Photography" job_type="8">Photography</option>
                 {/* <option value="Photographer" job_type="8">Photographer</option> */}
