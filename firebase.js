9782e54e84ffcdb0553c75a8bbd438e7b0ff446c
Application-Saab <harsha10889@gmail.com>
2025-12-29 18:55:12 +0530

    notification issue

--- firebase.js
+++ firebase.js
@@ -4,13 +4,12 @@ import { getMessaging } from 'firebase/messaging';
 
 // TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
-   apiKey: "AIzaSyD8mkyjHXX_fGcdENJJnU3GWI60YWMItl0",
-  authDomain: "wonderland-inapp-chat.firebaseapp.com",
-  projectId: "wonderland-inapp-chat",
-  storageBucket: "wonderland-inapp-chat.firebasestorage.app",
-  messagingSenderId: "336745779010",
-  appId: "1:336745779010:web:0f2125b937da40189942db",
-  measurementId: "G-QF6S6NZQL6"
+  apiKey: "AIzaSyAeV8b6f45_3c2ksc69Xx-U4Zy6yKcxo6o",
+  authDomain: "hora-379ab.firebaseapp.com",
+  projectId: "hora-379ab",
+  storageBucket: "hora-379ab.firebasestorage.app",
+  messagingSenderId: "545787711672",
+  appId: "1:545787711672:web:47fe2f098e71936033a44d"
   };
 
 // Initialize Firebase
