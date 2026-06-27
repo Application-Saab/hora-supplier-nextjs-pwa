eebbedda7c138f0b29dae1545cdc0d0cd486fe6e
BhavnaGurjar <bhavnag340@gmail.com>
2026-06-27 18:15:57 +0530

    disable the accept order button when limit reached check by backend endpoint

--- component/OrderDetailTab/index.jsx
+++ component/OrderDetailTab/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import OrderDetailsMenu from "../OrderDetailsMenu";
 import OrderDetailsIngre from "../OrderDetailsIngre";
 import OrderDetailsAppliances from "../OrderDetailsAppliances";
@@ -9,6 +9,7 @@ import {
 } from "../../apiconstant/apiconstant";
 import DecorationOrderDetailsTab from "../decorationOrderDetailsTab";
 import PhotographyOrderDetailsTab from "../photographyOrderDetailsTab";
+import axios from "axios";
 
 const OrderDetailTab = ({
   orderDetail,
@@ -33,6 +34,8 @@ const OrderDetailTab = ({
     var supplierID = localStorage.getItem("supplierID");
   }
   const [tab, setTab] = useState("Menu");
+  const [limitData, setLimitData] = useState(false);
+  const [loadingLimit, setLoadingLimit] = useState(true);
   const [orderStatus, setOrderStatus] = useState(orderDetail?.order_status);
 
 
@@ -103,7 +106,46 @@ const OrderDetailTab = ({
     }
   };
 
+
+  useEffect(() => {
+    const targetDate = orderDetail?.order_date;
+
+    if (supplierID && targetDate) {
+      const fetchSupplierLimit = async () => {
+        try {
+          setLoadingLimit(true);
+          const response = await axios.get(`${BASE_URL}/api/users/supplier-order-count-by-date`, {
+            params: {
+              supplierId: supplierID,
+              fulfillmentDate: targetDate
+            }
+          });
+
+          if (response?.data?.success) {
+            setLimitData(response?.data?.isFull);
+          }
+        } catch (error) {
+          console.error("Error fetching supplier daily limit:", error);
+        }
+        finally {
+          setLoadingLimit(false); 
+        }
+      };
+
+      fetchSupplierLimit();
+    }
+    else {
+      setLoadingLimit(false);
+    }
+  }, [supplierID, orderDetail]);
+
   return ( 
+    <>
+      {loadingLimit ? 
+      <div>
+        loading data...
+      </div>
+      :
     <>
       {parseInt(orderType) == 2 ? (
         <div>
@@ -219,9 +261,14 @@ const OrderDetailTab = ({
       ) : null}
 
       <div className="accept-btn-container" onClick={acceptOrder}>
-        <button className="acceptOrder acceptbutton">Accept Order</button>
+        <button
+          disabled={limitData}
+         className="acceptOrder acceptbutton">Accept Order</button>
       </div>
     </>
+}
+    </>
+
   );
 };
 
